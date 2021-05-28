4673a74635feea5cc5ccfd57d302c6115ff1ca12
LiliyaIngul <liliyaingul@gmail.com>
2021-05-28 16:52:07 -0400

    add questionary

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import BootstrapVue from 'bootstrap-vue';
+import Vue from 'vue';
+
+import App from './App.vue';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
